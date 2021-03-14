050cbc942dbb909d69a8b18c892593d063ae53da
Brian Monroy <brian.gmonroy98@gmail.com>
2021-03-14 17:33:19 -0300

    traversin the dom

---
+++ 13-DOM/js/08-scripts.js
@@ -0,0 +1,33 @@
+//Traversing de padre a hijo
+
+const navegacion = document.querySelector('.navegacion')
+console.log(navegacion)
+console.log(navegacion.children) //nodos
+
+for(i=0;i<navegacion.children.length;i++){
+    
+    console.log(`[${i}]:${navegacion.children[i].nodeType}`)
+}
+
+const card = document.querySelector(".card")
+
+card.children[1].children[1].textContent = "Nuevo heading desde traversing the dom"
+
+card.children[0].src = "img/hacer3.jpg"
+
+//Traversing de hijo al padre.
+
+console.log(card.parentNode)
+console.log(card.parentElement.parentElement.parentElement)
+
+//Siguiente elemento (hermano)
+
+console.log(card.nextElementSibling)
+
+const ultimoCard = document.querySelector('.card:nth-child(4)')
+
+console.log(ultimoCard)
+console.log(ultimoCard.previousElementSibling)
+
+console.log(navegacion.firstElementChild)
+console.log(navegacion.lastElementChild)
