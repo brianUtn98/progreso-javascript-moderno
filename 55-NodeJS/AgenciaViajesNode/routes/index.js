2069f3b769cf98ad4eaacaa12c4ad7d737cc02e0
Brian Monroy <brian.gmonroy98@gmail.com>
2021-03-25 00:31:33 -0300

    comienzo con nodeJS, express

---
+++ 55-NodeJS/AgenciaViajesNode/routes/index.js
@@ -0,0 +1,17 @@
+import express from "express";
+
+const router = express.Router();
+
+router.get("/", (request, response) => {
+  response.send("Inicio");
+});
+
+router.get("/nosotros", (request, response) => {
+  response.send("Nosotros");
+});
+
+router.get("/contacto", (request, response) => {
+  response.send("Contacto");
+});
+
+export default router;
